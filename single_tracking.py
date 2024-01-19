2fc9027baedeafdb9fe16c2e277191af0628964a
Setup Automação Mateus Quitério Marques <mateus.marques@setup.com.br>
2024-01-18 23:51:56 -0300

    implementadoo 7 algoritmos de rastreamentos

--- single_tracking.py
+++ single_tracking.py
@@ -1,68 +1,78 @@
-# ------------------- instalação de pacotes ------------------- #
-# pip install opencv-python==4.9.0
-# pip install opencv-contrib-python==4.9.0
-# ------------------------------------------------------------- #
-
-# importar pacotes #
 import cv2
 import sys
 from random import randint
 
-# Versão Opencv #
 (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+#print(major_ver, minor_ver, subminor_ver)
 
-# Tipos de rastreamentos #
-tracker_types = ['BOOSTING', 
-                 'MIL',
-                 'KCF',
-                 'TLD',
-                 'MEDIANFLOW',
-                 'MOSSE', 
-                 'CSRT']
+tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'MOSSE', 'CSRT']
+tracker_type = tracker_types[6]
 
-tracker_type = tracker_types[1]
-
-# Selecionador do tipo de rastreamento #
-if int(minor_ver) < 3 :
+if int(minor_ver) < 3:
     tracker = tracker_type
 else:
-    if tracker_type == 'BOOSTING':
-        tracker = cv2.legacy.TrackerBoosting_create()
-    if tracker_type == 'MIL':
-        tracker = cv2.legacy.TrackerMIL_create()
-    if tracker_type == 'KCF':
-        tracker = cv2.legacy.TrackerKCF_create()
-    if tracker_type == 'TLD':
-        tracker = cv2.legacy.TrackerTLD_create()
-    if tracker_type == 'MEDIANFLOW':
-        tracker = cv2.legacy.TrackerMedianFlow_create()
-    if tracker_type == 'MOSSE':
-        tracker = cv2.legacy.TrackerMOSSE_create()
-    if tracker_type == 'CSRT':
-        tracker = cv2.legacy.TrackerCSRT_create()
+   if tracker_type == 'BOOSTING':
+       tracker = cv2.legacy.TrackerBoosting_create()
+   if tracker_type == 'MIL':
+       tracker = cv2.TrackerMIL_create()
+   if tracker_type == 'KCF':
+       tracker = cv2.TrackerKCF_create()
+   if tracker_type == 'TLD':
+       tracker = cv2.legacy.TrackerTLD_create()
+   if tracker_type == 'MEDIANFLOW':
+       tracker = cv2.legacy.TrackerMedianFlow_create()
+   if tracker_type == 'MOSSE':
+       tracker = cv2.legacy.TrackerMOSSE_create()
+   if tracker_type == 'CSRT':
+       tracker = cv2.TrackerCSRT_create()
 
-# Carregar o video #
-video = cv2.VideoCapture('Videos/race.mp4')
-# Verificar se o video esta disponivel #
+#print(tracker)
+
+video = cv2.VideoCapture('videos/race.mp4')
 if not video.isOpened():
-    print('Não foi possivel encontrar o vídeo')
+    print('Não foi possível carregar o vídeo')
     sys.exit()
 
-# Ler os frames dos videos #
 ok, frame = video.read()
-# Verifica se o vídeo não está corrompido #
 if not ok:
-    print('Não foi possivel ler o arquivo do video')
+    print('Não foi possível ler o arquivo de vídeo')
     sys.exit()
+#print(ok)
 
-# Seleciona a região de interesse
 bbox = cv2.selectROI(frame, False)
 #print(bbox)
 
-#Inicializa o tracker
 ok = tracker.init(frame, bbox)
 #print(ok)
 
-# Random de cores para o bbox
-colors = (randint(0,255), randint(0, 255), randint(0, 255))
+colors = (randint(0, 255), randint(0, 255), randint(0, 255))
 #print(colors)
+
+while True:
+    ok, frame = video.read()
+    if not ok:
+        break
+
+    # https://docs.opencv.org/master/dc/d71/tutorial_py_optimization.html
+    timer = cv2.getTickCount()
+    ok, bbox = tracker.update(frame)
+    #print(ok, bbox)
+
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+
+    if ok:
+        (x, y, w, h) = [int(v) for v in bbox]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), colors, 2, 1)
+    else:
+        cv2.putText(frame, 'Falha no rastreamento', (100, 80),
+                    cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 0, 255), 2)
+
+    cv2.putText(frame, tracker_type + ' Tracker', (100, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, .75, (50, 170, 50), 2)
+
+    cv2.putText(frame, 'FPS: ' + str(int(fps)), (100, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, .75, (50, 170, 50), 2)
+
+    cv2.imshow('Tracking', frame)
+    if cv2.waitKey(1) & 0XFF == 27:
+        break
