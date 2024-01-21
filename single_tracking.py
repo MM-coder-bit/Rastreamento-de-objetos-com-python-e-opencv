c496c6f54a1247450b0767a4b6473d872d8502db
Setup Automação Mateus Quitério Marques <mateus.marques@setup.com.br>
2024-01-21 16:41:04 -0300

    comentado os códigos single e multi tracker

--- single_tracking.py
+++ single_tracking.py
@@ -1,3 +1,13 @@
+# -------------------------------------------------------------------------------------------------------------------------------#
+# Rastreamento de Objeto com OpenCV
+
+# Este código demonstra como usar o OpenCV para realizar o rastreamento de objeto em uma sequência de vídeo.
+# O código utiliza a função `tracker.init()` para criar um objeto tracker,
+# que pode ser usado para rastrear o objeto em um vídeo.
+# -------------------------------------------------------------------------------------------------------------------------------#
+
+# -------------------------------------------------------------------------------------------------------------------------------#
+# Importar as bibliotecas necessárias
 import cv2
 import sys
 from random import randint
@@ -5,9 +15,21 @@ from random import randint
 (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 #print(major_ver, minor_ver, subminor_ver)
 
+# Definir os tipos de rastreadores
+
+# O código define uma lista de tipos de rastreadores que podem ser usados pelo objeto multitracker. Esses tipos incluem:
+# `BOOSTING`
+# `MIL`
+# `KCF`
+# `TLD`
+# `MEDIANFLOW`
+# `MOSSE`
+# `CSRT`
 tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'MOSSE', 'CSRT']
 tracker_type = tracker_types[6]
 
+# -------------------------------------------------------------------------------------------------------------------------------#
+# Condição para criar o rastreador
 if int(minor_ver) < 3:
     tracker = tracker_type
 else:
@@ -26,28 +48,41 @@ else:
    if tracker_type == 'CSRT':
        tracker = cv2.TrackerCSRT_create()
 
-#print(tracker)
+# -------------------------------------------------------------------------------------------------------------------------------#
+# Criar um objeto de captura de vídeo
 
+# O código cria um objeto de captura de vídeo usando a função `cv2.VideoCapture()`.
+# O objeto de captura de vídeo é usado para ler frames de um arquivo de vídeo.
 video = cv2.VideoCapture('videos/race.mp4')
 if not video.isOpened():
     print('Não foi possível carregar o vídeo')
     sys.exit()
 
+# Ler o primeiro frame do vídeo
+
+# O código lê o primeiro frame do vídeo usando o método `read()` do objeto de captura de vídeo.
+# Se o frame não for lido com sucesso, o código imprime uma mensagem de erro e sai.
 ok, frame = video.read()
 if not ok:
     print('Não foi possível ler o arquivo de vídeo')
     sys.exit()
-#print(ok)
 
+# -------------------------------------------------------------------------------------------------------------------------------#
+# Selecionar caixa delimitadora para o objeto a ser rastreado
+
+# O código usa a função `cv2.selectROI()` para permitir que o usuário selecione caixa delimitadora para o objeto a ser rastreado.
+# A caixa delimitadora é armazenada em uma variável chamada `bboxes`.
 bbox = cv2.selectROI(frame, False)
-#print(bbox)
 
+# Inicializar o rastreador de objetos
 ok = tracker.init(frame, bbox)
-#print(ok)
 
-colors = (randint(0, 255), randint(0, 255), randint(0, 255))
-#print(colors)
+colors = (randint(0, 255), randint(0, 255), randint(0, 255)) # Gerar cores aleatórias para as caixas delimitadoras
 
+# Detecta o objeto em todo o vídeo
+
+# Enquanto rastreia o objeto a cada frame, é apresentado dados de 'fps' e
+# posição da caixa delimitadora na tela.
 while True:
     ok, frame = video.read()
     if not ok:
@@ -56,8 +91,7 @@ while True:
     # https://docs.opencv.org/master/dc/d71/tutorial_py_optimization.html
     timer = cv2.getTickCount()
     ok, bbox = tracker.update(frame)
-    #print(ok, bbox)
-
+    
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
 
     if ok:
